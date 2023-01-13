4871e7022a686405f3a711b9b3cc19cded590bbf
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-01-13 12:12:31 +0300

    first commit

---
+++ SwiftUI-Firebase-LoginScreen/SwiftUI_Firebase_LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_LoginScreenApp.swift
+//  SwiftUI-Firebase-LoginScreen
+//
+//  Created by Mehmet Kerim ÖZEK on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
